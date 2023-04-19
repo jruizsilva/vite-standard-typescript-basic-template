c83b65ecc9b1f6b4b45d87d19572d2a880685596
Jonathan Ruiz <ruizsilvajonathan@gmail.com>
2023-04-19 03:08:24 -0300

    new rules added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,28 @@ module.exports = {
     '@typescript-eslint/consistent-type-definitions': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn'
+    '@typescript-eslint/no-unused-vars': 'warn',
+    
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+    'react/self-closing-comp': 'warn',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true
+      }
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var']
+      }
+    ]
   }
 }
